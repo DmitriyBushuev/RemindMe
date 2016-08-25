eee676770c226bc5c950b3d35ee45d21d0bfe445
Dmitriy <dimasikbb@g,ail.com>
2016-08-25 22:13:30 +0300

    Created toolbar and search button

--- app/src/main/java/com/dimasik/remindme/RemindMeMainActivity.java
+++ app/src/main/java/com/dimasik/remindme/RemindMeMainActivity.java
@@ -1,13 +1,35 @@
 package com.dimasik.remindme;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class RemindMeMainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initToolBar();
+    }
+
+    private void initToolBar()
+    {
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.app_name);
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                Toast.makeText(RemindMeMainActivity.this,"Search",Toast.LENGTH_SHORT).show();
+                return true;
+            }
+        });
+        toolbar.inflateMenu(R.menu.menu);
     }
 }
